96714578094ddbc21d2e2ce412caed7d4438af19
Ilya Sachok <isachek2001@gmail.com>
2021-12-23 21:30:52 +0300

    Added correct requests to oracle, code refactor

--- main.py
+++ main.py
@@ -1,47 +1,47 @@
-import subprocess
 import json
 import time
-import ast
-import os
 import requests
+import random
 
-get_role = 'user'
-get_action = 'get_requests'
-cid = '19bda81c54f0e578cffe31b369067ff3f4581f96f6d4c43cc61d43fb8a8d49e2'
-b = 'cd /home/ilya/'
-base_command = 'cd "/home/ilya/Downloads/linux-beam-wallet-cli-masternet-6.1.11422 (1)/beam-wallet-masternet/";' \
-               + f' ./beam-wallet-masternet shader --shader_app_file=app.wasm --shader_args="role={get_role},' \
-               + f'action={get_action},cid={cid}" -n 127.0.0.1:10007'
+app_file = '/home/ilya/BeamWork/SomeShitForNftGallery/randomoracle/build/wasi/shaders/app.wasm'
+cid = '335a8471e06d986cb287065445d402f93e201a752ca161fd23bff4092279e9ec'
+wallet_api_address = 'http://0.0.0.0:10011/api/wallet'
 
-# while True:
-# s = ''
-# s = subprocess.check_output(['ls'])
-# # json.loads(s)
-# print(s)
 
-# process = subprocess.Popen(base_command.split(), shell=True, stdout=subprocess.PIPE)
-# time.sleep(5)
-# stdout = process.communicate()
-# for s in stdout:
-#     print('STDOUT:{}'.format(s))
-# os.system(base_command)
+class GalleryRequest:
+    def __init__(self, user_key, id):
+        self.user_key = user_key
+        self.id = id
 
-# "contract_file":"/your contract/",
 
+def set_oracle_value(gallery_request_to_fulfill, value):
+    requests.get(wallet_api_address,
+                 data='{"jsonrpc":"2.0","id":"get_requests","method":"invoke_contract","params":'
+                      + '{{"contract_file":"{app_file}","args":'.format(app_file=app_file)
+                      + '"role=user,action=save_value,cid={cid},value={value},key={key},id={id}"}}}}'.format(
+                     cid=cid,
+                     value=value,
+                     key=gallery_request_to_fulfill.user_key,
+                     id=gallery_request_to_fulfill.id)
+                 )
+
+
+gallery_requests = dict()
 while True:
-    r = requests.get('http://0.0.0.0:10000/api/wallet',
-                     # data='{"jsonrpc":"2.0","id":"get_requests","method":"invoke_contract","params":'
-                     #      + '{"contract_file":"/home/ilya/BeamWork/SomeShitForNftGallery/randomoracle/build/wasi/shaders/app.wasm","args":'
-                     #      + '"role=user,action=get_oracle_requests,cid=335a8471e06d986cb287065445d402f93e201a752ca161fd23bff4092279e9ec"}}'
-                     data='{"jsonrpc":"2.0","id":"view_contracts","method":"invoke_contract","params":{"contract_file":"/home/ilya/BeamWork/SomeShitForNftGallery/randomoracle/build/wasi/shaders/app.wasm","args":"role=manager,action=view_contracts"}}'
+    r = requests.get(wallet_api_address,
+                     data='{"jsonrpc":"2.0","id":"get_requests","method":"invoke_contract","params":'
+                          + '{{"contract_file":"{app_file}","args":'.format(app_file=app_file)
+                          + '"role=user,action=get_oracle_requests,cid={cid}"}}}}'.format(cid=cid)
                      )
 
-    # current_requests = json.loads(r.text())#['result']['output']['values']
-
-    values = ast.literal_eval(json.loads(json.dumps(r.json()))['result']['output'].split(' ')[1][:-1])
-
-    if len(values) != 0:
-        for value in values:
-            print(value)
+    b = '{' + r.json()['result']['output'] + '}'
+    parsed_requests = json.loads(b)['values']
+    for parsed_request in parsed_requests:
+        gallery_request = GalleryRequest(user_key=parsed_request[0]['Value Info']['value.request_id.requester_key'],
+                                         id=parsed_request[0]['Value Info']['value.request_id.id_in_requester'])
+        if gallery_request not in gallery_requests:
+            n = random.randint(0, 36893488147419103231)
+            gallery_requests[gallery_request] = n
+            set_oracle_value(gallery_request, n)
 
     time.sleep(5000)
